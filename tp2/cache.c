bdb1de42b5ff9342c69ff176fb682bd3fcc40d50
Jonathan Medina <jonathan.medina@despegar.com>
2020-07-22 23:59:23 -0300

    Unified stuff

--- tp2/cache.c
+++ tp2/cache.c
@@ -2,8 +2,20 @@
 // Created by christianflorez on 1/7/20.
 //
 #include "cache.h"
-#include "lista.c"
 #include "binario.h"
+
+
+typedef struct nodo{
+    bloqueCache_t* dato;
+    struct nodo* prox;
+} nodo_t;
+
+
+struct lista{
+    nodo_t* prim;
+    nodo_t* ult;
+    size_t len;
+};
 
 int compare_tag(unsigned int tag, unsigned int set) {
     conjunto_t* conjunto = associative_cache.conjuntos[set];
@@ -46,7 +58,6 @@ void read_tocache(unsigned int blocknum, unsigned int way, unsigned int set) {
 }
 
 unsigned char read_byte(unsigned int address) {
-
     unsigned int offset, set, tag, way, blocknum;
 
     offset = get_offset(address);
@@ -92,17 +103,96 @@ void write_byte(unsigned int address, unsigned char value) {
 void init() {
     associative_cache.amount_access = 0;
     associative_cache.amount_misses = 0;
-    for (int i=0; i<4; i++) {
-        way_t* way = malloc(sizeof(way_t));
-        for (int j=0; j<8; j++) {
-            conjunto_t *conjunto = malloc(sizeof(conjunto_t));
-            conjunto->listaEnlazada = lista_crear();
-            way->conjuntos[j] = conjunto;
-        }
-        associative_cache.ways[i] = way;
+    for (int i=0; i<8; i++) {
+        conjunto_t *conjunto = malloc(sizeof(conjunto_t));
+        conjunto->listaEnlazada = lista_crear();
+        associative_cache.conjuntos[i] = conjunto;
     }
 
     for (int i=0; i<MAIN_MEMORY_SIZE; i++) {
         memoriaPrincipal.memoria[i] = malloc(sizeof(bloqueDeMemoria_t));
     }
+}
+
+nodo_t* crear_nodo(bloqueCache_t* valor){
+    nodo_t* nodo = malloc(sizeof(nodo_t));
+    if (!nodo) return NULL;
+
+    nodo->dato = valor;
+    nodo->prox = NULL;
+
+    return nodo;
+}
+
+lista_t *lista_crear(void){
+    lista_t* lista = malloc(sizeof(lista_t));
+    if (lista==NULL) return NULL;
+    lista->prim = NULL;
+    lista->ult = NULL;
+    lista->len = 0;
+    return lista;
+}
+
+void lista_destruir(lista_t *lista){
+    while (!(lista_esta_vacia(lista))){
+        lista_borrar_primero(lista);
+    }
+    free(lista);
+}
+
+
+void *lista_borrar_primero(lista_t *lista){
+    if(lista_esta_vacia(lista)) return NULL;
+    void* dato = lista->prim->dato;
+    nodo_t* prox_nodo = lista->prim->prox;
+    free(lista->prim);
+    lista->prim = prox_nodo;
+    if (prox_nodo == NULL)
+        lista->ult = NULL;
+    lista->len--;
+    return dato;
+}
+
+bool lista_insertar_primero(lista_t *lista, bloqueCache_t* dato){
+    nodo_t* nodo = crear_nodo(dato);
+    if (nodo == NULL) return false;
+    nodo_t* nodo_prim = lista->prim;
+    nodo->prox = nodo_prim;
+    lista->prim = nodo;
+    if (nodo_prim == NULL) lista->ult = nodo;
+    lista->len++;
+    return true;
+}
+
+bool lista_insertar_ultimo(lista_t *lista, bloqueCache_t *dato){
+    nodo_t* nodo = crear_nodo(dato);
+    if (nodo == NULL) return false;
+    nodo_t* nodo_ult = lista->ult;
+    if (nodo_ult == NULL){
+        lista->prim = nodo;
+    }
+    else{
+        nodo_ult->prox = nodo;
+    }
+    lista->ult = nodo;
+    lista->len++;
+    return true;
+}
+
+bool lista_esta_vacia(const lista_t *lista){
+    return lista->len == 0;
+}
+
+size_t lista_largo(const lista_t *lista){
+    return lista->len;
+}
+
+void *lista_ver_primero(const lista_t *lista){
+    if (lista_esta_vacia(lista)) return NULL;
+    return lista->prim->dato;
+}
+
+void *lista_ver_ultimo(const lista_t* lista){
+    if (lista_esta_vacia(lista)) return NULL;
+    return lista->ult->dato;
 }
--- tp2/cache.h
+++ tp2/cache.h
@@ -1,21 +1,27 @@
 #ifndef TP2_CACHE_H
 #define TP2_CACHE_H
-#include "lista.h"
 #include "binario.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
 #define CACHE_BLOCK_SIZE 128
 #define MAIN_MEMORY_SIZE 512
 
-typedef unsigned char bloqueDeMemoria_t[CACHE_BLOCK_SIZE];
 
-typedef struct memoriaPrincipal {
-    bloqueDeMemoria_t memoria[MAIN_MEMORY_SIZE];
-} memoriaPrincipal_t;
+struct lista;
+typedef struct lista lista_t;
+typedef unsigned char bloqueDeMemoria_t[CACHE_BLOCK_SIZE];
 
 typedef struct bloqueCache {
     int V;
     int tag;
     bloqueDeMemoria_t datos;
 } bloqueCache_t;
+
+typedef struct memoriaPrincipal {
+    bloqueDeMemoria_t memoria[MAIN_MEMORY_SIZE];
+} memoriaPrincipal_t;
+
 
 typedef struct conjunto {
     lista_t* listaEnlazada;
@@ -38,6 +44,60 @@ memoriaPrincipal_t memoriaPrincipal;
 unsigned char read_byte(unsigned int address);
 void write_byte(unsigned int address, unsigned char value);
 float get_miss_rate();
+int compare_tag(unsigned int tag, unsigned int set);
+unsigned int select_oldest(unsigned int setnum);
+void read_tocache(unsigned int blocknum, unsigned int way, unsigned int set);
+
 void init();
+
+// ------------------------------------
+
+//PRIMITIVAS LISTA ENLAZADA
+
+// Crea una lista enlazada.
+// Post: devuelve una nueva lista enlazada vacía.
+lista_t *lista_crear(void);
+
+// Devuelve verdadero o falso, según si la lista tiene o no elementos.
+// Pre: la lista enlazada fue creada.
+bool lista_esta_vacia(const lista_t *lista);
+
+// Agrega un elemento al principio de la lista. Devuelve Falso en caso de error.
+// Pre: La lista fue creada
+// Post: Se ha agrego un elemento al principio de la lista.
+bool lista_insertar_primero(lista_t *lista, bloqueCache_t* dato);
+
+// Agrega un elemento al final de la lista. Devuelve Falso en caso de error.
+// Pre: La lista fue creada
+// Post: Se ha agregado un elemento al final de la lista.
+bool lista_insertar_ultimo(lista_t *lista, bloqueCache_t* dato);
+
+// Saca el primer elemento de la lista, y se devuelve su valor, si está vacía, devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el valor del primer elemento anterior, la lista
+// contiene un elemento menos, si la lista no estaba vacía.
+void *lista_borrar_primero(lista_t *lista);
+
+// Devuelve el primer elemento de la lista. Devuelve NULL en caso de no haber elementos.
+// Pre: La lista fue creada
+// Post: se devolvio el primer elemento de la lista
+void *lista_ver_primero(const lista_t *lista);
+
+// Devuelve el ultimo elemento de la lista. Devuelve NULL en caso de no haber elementos.
+// Pre: La lista fue creada
+// Post: Se devolvio el ultimo elemento de la lista
+void *lista_ver_ultimo(const lista_t* lista);
+
+// Devuelve el largo de la lista enlazada.
+// Pre: La lista fue creada.
+// Post: Se devolvio el largo de la lista enlazada
+size_t lista_largo(const lista_t *lista);
+
+// Destruye la lista. Si se recibe la función destruir_dato por parametro,
+// para cada uno de los elementos de la lista llama a destruir_dato.
+// Pre: la lista fue creada. destruir_dato es una función capaz de destruir
+// los datos de la lista, o NULL en caso de que no se la utilice.
+// Post: se eliminaron todos los elementos de la lista.
+void lista_destruir(lista_t *lista);
 
 #endif //TP2_CACHE_H
